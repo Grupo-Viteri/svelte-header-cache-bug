08af9821251b894559b4a5b88f822b2a4f35d451
Ricardo Viteri <rviteri@estratega.ec>
2024-01-18 21:08:45 -0500

    modified comments about error

--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	/*buggy code: if import fails to load here the console prints Error: "Cache-Control" header is already set but
-    there is no information about the error that caused this. 
+	/*buggy code: if import fails to load the console prints Error: "Cache-Control" header is already set 
+    however, there is no information about the error that caused this. 
     */
 
 	import * as Card from 'card-validator';
 
 	/*
-    Comment the previous import and uncomment the import below and the error goes away.
+    Comment the previous import and uncomment the import below and the error goes away when the page is loaded.
     */
 	//import Card from 'card-validator';
 
